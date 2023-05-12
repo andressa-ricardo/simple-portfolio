868d854988000d363334d0b74ed7aebad7423038
andressa-ricardo <andressaricardo30@gmail.com>
2023-05-11 21:10:14 -0300

    alteracao no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,24 +7,26 @@ export function Footer() {
   return (
     <>
       <div className={footer}>
-        <a href="https://www.linkedin.com/in/andressa-ricardo/">
-          <button>
-            <img src={linkedin} alt="" />
-            <p>Linkedin</p>
-          </button>
-        </a>
-        <a href="https://github.com/andressa-ricardo">
-          <button>
-            <img src={github} alt="" />
-            <p>Github</p>
-          </button>
-        </a>
-        <a href="mailto:andressaricardo30@gmail.com">
-          <button>
-            <img src={email} alt="" />
-            <p>Email</p>
-          </button>
-        </a>
+        <div>
+          <a href="https://www.linkedin.com/in/andressa-ricardo/">
+            <button>
+              <img src={linkedin} alt="" />
+              <p>Linkedin</p>
+            </button>
+          </a>
+          <a href="https://github.com/andressa-ricardo">
+            <button>
+              <img src={github} alt="" />
+              <p>Github</p>
+            </button>
+          </a>
+          <a href="mailto:andressaricardo30@gmail.com">
+            <button>
+              <img src={email} alt="" />
+              <p>Email</p>
+            </button>
+          </a>
+        </div>
       </div>
     </>
   );
